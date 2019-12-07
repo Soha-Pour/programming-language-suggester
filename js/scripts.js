5908ce4c90958d398f234360576c2d1d4f748a7f
Soha Pourpirali <pourpirali.soha@gmail.com>
2019-12-07 12:23:07 -0800

    add forms for travel and free time with corresponding $

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
     var meal = $("input:radio[name=meal]:checked").val();
     var show = $("input:radio[name=show]:checked").val();
     var marathon = parseInt($("#marathon").val());
+    var buddy = parseInt($("#buddy").val());
+    var motivation = $("input:radio[name=motivation]:checked").val();
+    var free = $("input:radio[name=free]:checked").val();
 
   if (meal === "lobster-roll") {
     $("#ruby").show();
@@ -17,5 +20,17 @@ $(document).ready(function() {
     $("#js").show();
   }
 
+  if (buddy === 1) {
+    $("#r").show();
+  }
+
+  if (motivation === "money") {
+    $("#go").show();
+  }
+
+  if (free === "sleep") {
+    $("#ruby").show();
+  }
+  
     });
   });
