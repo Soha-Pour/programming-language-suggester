08c43c1e14ba9c10d1f16dd667f039565569b1cc
Soha Pourpirali <pourpirali.soha@gmail.com>
2019-12-07 19:40:32 -0800

    add hide functionality to make new responses replace old ones.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   $("form#blanks").submit(function(event) {
     event.preventDefault();
-    $(".hidden").hide();
-
+    
     var meal = $("input:radio[name=meal]:checked").val();
     var show = $("input:radio[name=show]:checked").val();
     var marathon = parseInt($("#marathon").val());
     var buddy = parseInt($("#buddy").val());
     var motivation = $("input:radio[name=motivation]:checked").val();
     var free = $("input:radio[name=free]:checked").val();
+    $("#ruby, #python, #js, #go, #r").hide();
 
     if (meal === "burger" && show === "office" && marathon === 1 && buddy === 1 && motivation === "peace" && free === "sleep") {
       $("#ruby").show();
@@ -21,8 +21,8 @@ $(document).ready(function() {
     } else if (meal === "burrito" && show === "office" && marathon === 2 && buddy === 4 && motivation === "money" && free === "closet") {
       $("#r").show();
     } else {
-      $("#ruby").show();
+      $("#r").show();
     }
-    
+
     });
   });
