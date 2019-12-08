eebbe5955eb922560925dfe2037f7a1b04df9b5d
Soha Pourpirali <pourpirali.soha@gmail.com>
2019-12-07 19:53:06 -0800

    add name input field to address the user directly

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   $("form#blanks").submit(function(event) {
     event.preventDefault();
-    
+
     var meal = $("input:radio[name=meal]:checked").val();
     var show = $("input:radio[name=show]:checked").val();
     var marathon = parseInt($("#marathon").val());
     var buddy = parseInt($("#buddy").val());
     var motivation = $("input:radio[name=motivation]:checked").val();
     var free = $("input:radio[name=free]:checked").val();
+    var nameInput = $("input#name").val();
     $("#ruby, #python, #js, #go, #r").hide();
 
     if (meal === "burger" && show === "office" && marathon === 1 && buddy === 1 && motivation === "peace" && free === "sleep") {
@@ -23,6 +24,8 @@ $(document).ready(function() {
     } else {
       $("#r").show();
     }
+
+    $(".name").text(nameInput);
 
     });
   });
